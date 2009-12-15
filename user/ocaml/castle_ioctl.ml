9b520a6f72aeec797427a30dfd213b58da59f4b6
Tom Wilkie <tom@acunu.com>
2009-12-15 15:42:42 +0000

    Updated ocaml lib

--- user/ocaml/castle_ioctl.ml
+++ user/ocaml/castle_ioctl.ml
@@ -1,10 +1,12 @@
 
-external castle_ioctl : Unix.file_descr -> int32 -> int32 = "castle_ioctl"
+external castle_ioctl : Unix.file_descr -> int32 -> int64 -> int32 = "castle_ioctl"
 
-let cmd = Int32.of_string (Sys.argv.(1));;
+let cmd = Int32.of_string (Sys.argv.(1))
 
-let fh = Unix.openfile "/proc/xen/privcmd" [Unix.O_RDWR] 0;;
+let arg = Int64.of_string (Sys.argv.(2))
 
-let result = castle_ioctl fh cmd;;
+let fh = Unix.openfile "/dev/castle/control" [Unix.O_RDWR] 0;;
+
+let result = castle_ioctl fh cmd arg;;
 
 Printf.printf "result: %i"
