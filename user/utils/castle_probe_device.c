d8986928feec93b0bf7dbfd60250f4bc050bd24d
Christian Sellars <csellars@acunu.com>
2011-07-11 13:21:27 +0100

    Use DIRECT_IO in castle_probe_device.

--- user/utils/castle_probe_device.c
+++ user/utils/castle_probe_device.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <errno.h>
@@ -29,11 +30,15 @@ static int castle_fs_superblock_validate(struct castle_fs_superblock_public *fs_
     return 0;
 }
 
+#define ROUND_UP_ALIGN(x, align) ((x + align) - 1) & ~(align - 1)
+
 int main(int argc, char *argv[])
 {
     int    fd, ret;
-    struct castle_slave_superblock_public cs_sb;
-    struct castle_fs_superblock_public fs_sb;
+    struct castle_slave_superblock_public *cs_sb;
+    struct castle_fs_superblock_public *fs_sb;
+    size_t cs_sb_alignbuf_size, fs_sb_alignbuf_size;
+    unsigned int align;
 
     if (argc != 2)
     {
@@ -41,8 +46,27 @@ int main(int argc, char *argv[])
       return 2;
     }
 
-    if ((fd = open(argv[1], O_RDONLY | O_NOCTTY)) < 0) {
+    if ((fd = open(argv[1], O_RDONLY | O_NOCTTY | O_DIRECT)) < 0) {
       fprintf(stderr, "Failed to open %s: %s\n", argv[1], strerror(errno));
+      return 1;
+    }
+
+    /* Find alignment requirements for posix_memalign / O_DIRECT read. */
+    align = pathconf(argv[1], _PC_REC_XFER_ALIGN);
+
+    cs_sb_alignbuf_size = ROUND_UP_ALIGN(sizeof(struct castle_slave_superblock_public), align);
+    ret = posix_memalign((void *)&cs_sb, align, cs_sb_alignbuf_size);
+    if (ret == -1)
+    {
+      perror("posix_memalign failed\n");
+      return 1;
+    }
+
+    fs_sb_alignbuf_size = ROUND_UP_ALIGN(sizeof(struct castle_fs_superblock_public), align);
+    ret = posix_memalign((void *)&fs_sb, align, fs_sb_alignbuf_size);
+    if (ret == -1)
+    {
+      perror("posix_memalign failed\n");
       return 1;
     }
 
@@ -53,13 +77,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (read(fd, &cs_sb, sizeof(cs_sb)) != sizeof(cs_sb))
+    if (read(fd, cs_sb, cs_sb_alignbuf_size) != cs_sb_alignbuf_size)
     {
         perror("Failed to read cs_sb");
         return 1;
     }
 
-    if ((ret = castle_slave_superblock_validate(&cs_sb)) < 0)
+    if ((ret = castle_slave_superblock_validate(cs_sb)) < 0)
     {
         fprintf(stderr, "Invalid disk superblock (at %d)\n", -ret);
         return 1;
@@ -72,24 +96,28 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (read(fd, &fs_sb, sizeof(fs_sb)) != sizeof(fs_sb))
+    if (read(fd, fs_sb, fs_sb_alignbuf_size) != fs_sb_alignbuf_size)
     {
         perror("Failed to read fs_sb");
         return 1;
     }
 
-    if (cs_sb.flags & CASTLE_SLAVE_NEWDEV) {
+    if (cs_sb->flags & CASTLE_SLAVE_NEWDEV) {
       fprintf(stdout, "New disk - no filesystem superblock\n");
     }
-    else if ((ret = castle_fs_superblock_validate(&fs_sb)) < 0)
+    else if ((ret = castle_fs_superblock_validate(fs_sb)) < 0)
     {
       fprintf(stderr, "Invalid filesystem superblock (at %d)\n", -ret);
     }
     else {
-      fprintf(stdout, "Filesystem uuid: 0x%x\n", fs_sb.uuid);
+      fprintf(stdout, "Filesystem uuid: 0x%x\n", fs_sb->uuid);
     }
 
-    fprintf(stdout, "Disk uuid: 0x%x%s\n", cs_sb.uuid, (cs_sb.flags & CASTLE_SLAVE_SSD) ? ", SSD" : "");
+    fprintf(stdout, "Disk uuid: 0x%x%s\n", cs_sb->uuid,
+            (cs_sb->flags & CASTLE_SLAVE_SSD) ? ", SSD" : "");
+
+    free(cs_sb);
+    free(fs_sb);
 
     close(fd);
 
