e9370ce7a4326358b442ac6bd059551cdc436004
Bhaskar Muppana <bhaskar@acunu.com>
2011-07-14 15:45:48 +0100

    Recommit fix for merged_iter_skip(). Got lost in merge.

--- kernel/castle_da.c
+++ kernel/castle_da.c
@@ -1629,6 +1629,7 @@ static void castle_ct_merged_iter_skip(c_merged_iter_t *iter,
                                        void *key)
 {
     struct component_iterator *comp_iter;
+    int i;
 
     debug_iter("%s:%p\n", __FUNCTION__, iter);
 
@@ -1638,14 +1639,18 @@ static void castle_ct_merged_iter_skip(c_merged_iter_t *iter,
     /* If we are skipping in the merged iterator, we are not able to inform the client
        about all duplicate entries. Check that the client isn't asking for it. */
     BUG_ON(iter->each_skip);
-    /* We expect the iterator to be skipped after prep_next() call (and not after next()).
-       Otherwise, we won't have an entry cached for some of the component iterator
-       and we will be unable to decided whether to skip in them.
-       prep_next() guarantees that each of the component iterator will either be
-       completed or there will be something cached for that iterator.
-       Also, all iterators should support skip functionality.
 
-       Go through the rbtree, and extract all the keys smaller than the key we
+    /* Call skip on lower level iterators, if the iterator is not cached. */
+    for(i=0; i<iter->nr_iters; i++)
+    {
+        comp_iter = iter->iterators + i;
+
+        BUG_ON(!comp_iter->iterator_type->skip);
+        if (!comp_iter->completed && !comp_iter->cached)
+            comp_iter->iterator_type->skip(comp_iter->iterator, key);
+    }
+
+    /* Go through the rbtree, and extract all the keys smaller than the key we
        are skipping to. */
     while((comp_iter = castle_ct_merged_iter_rbtree_min_get(iter)) &&
           (iter->btree->key_compare(comp_iter->cached_entry.k, key) < 0))
