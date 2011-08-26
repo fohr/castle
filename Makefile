2f082150e54e95f47c7b2351f833d7d82d32e2a7
Andrew Suffield <asuffield@acunu.com>
2011-08-26 11:24:58 +0100

    Move userspace tools to castle-tools.hg

--- Makefile
+++ Makefile
@@ -2,14 +2,11 @@ KVER=$(shell uname -r)
 
 all:
 	make -C kernel KVER=$(KVER)
-	make -C user 
 
 install:
 	make -C kernel install KVER=$(KVER)
-	make -C user install
-	
+
 clean:
 	make -C kernel clean
-	make -C user clean
 
 bs-install: install
--- user/Rules.mk
+++
@@ -1,7 +0,0 @@
-subdirs-all subdirs-clean subdirs-install:
-	@set -e; for subdir in $(SUBDIRS) $(SUBDIRS-y); do \
-		$(MAKE) subdir-$(patsubst subdirs-%,%,$@)-$$subdir; \
-	done
-
-subdir-all-% subdir-clean-% subdir-install-%:
-	$(MAKE) -C $* $(patsubst subdir-%-$*,%,$@)
