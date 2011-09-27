9bc307af75aa10fdeba80cb6cc62b4a088d64c23
Lewis Thompson <lewis@acunu.com>
2011-09-27 16:17:53 +0100

    Fix underflow in castle_printk()

--- kernel/castle_utils.c
+++ kernel/castle_utils.c
@@ -358,8 +358,8 @@ void castle_printk(c_printk_level_t level, const char *fmt, ...)
     tmp_bufp = &tmp_buf[len];
 
     va_start(args, fmt);
-    len = (c_byte_off_t) vscnprintf(tmp_bufp,
-            sizeof(tmp_buf) - len, fmt, args) + 1; /* +1 for '\0'$ */
+    len += (c_byte_off_t) vscnprintf(tmp_bufp,
+	    sizeof(tmp_buf) - len, fmt, args) + 1; /* +1 for '\0'$ */
     va_end(args);
 
     /* Serialise access to the ring buffer. */
