dc0ae055cf7b053dbf7749ee9ca6dd60e7216e45
Grzegorz Miłoś <gregor@acunu.com>
2012-02-22 17:48:42 +0000

    Add sysfs flag to expose the state of the ctrl prog. 0 - not running, 1 - running.

--- kernel/castle_ctrl_prog.c
+++ kernel/castle_ctrl_prog.c
@@ -34,6 +34,10 @@ static int castle_nugget_disabled = 0;
 module_param(castle_nugget_disabled, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(castle_nugget_disabled, "Don't send events to start new nugget");
 
+int castle_ctrl_prog_present(void)
+{
+    return (castle_ctrl_prog_state == CTRL_PROG_PRESENT);
+}
 
 static void castle_ctrl_prog_touch(void)
 {
--- kernel/castle_ctrl_prog.h
+++ kernel/castle_ctrl_prog.h
@@ -1,6 +1,7 @@
 #ifndef __CASTLE_CTRL_PROG_H__
 #define __CASTLE_CTRL_PROG_H__
 
+int castle_ctrl_prog_present(void);
 int  castle_ctrl_prog_ioctl(cctrl_ioctl_t *ioctl);
 
 int  castle_ctrl_prog_init (void);
