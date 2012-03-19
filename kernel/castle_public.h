b5728725d05a1826a62fc8065da9b66f7ad207f4
Tirath Ramdas <tramdas@acunu.com>
2012-03-19 09:33:16 +0000

    Updated CASTLE_SLAVE_VERSION due to changeset 1fa9ed4fcb17.

--- kernel/castle_public.h
+++ kernel/castle_public.h
@@ -968,7 +968,7 @@ struct castle_key_value_list {
 #define CASTLE_SLAVE_MAGIC1     (0x02061985)
 #define CASTLE_SLAVE_MAGIC2     (0x16071983)
 #define CASTLE_SLAVE_MAGIC3     (0x16061981)
-#define CASTLE_SLAVE_VERSION    (24)            /* Last changed by: GM */
+#define CASTLE_SLAVE_VERSION    (25)            /* Last changed by: TR */
 
 #define CASTLE_SLAVE_NEWDEV     (0x00000004)
 #define CASTLE_SLAVE_SSD        (0x00000008)
